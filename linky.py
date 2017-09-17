3512de1fb0e80f197e4a37a0e7fa1d5f3d65b876
epierre <epierre@e-nef.com>
2017-09-17 20:52:50 +0200

    updated

--- linky.py
+++ linky.py
@@ -1,9 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""Generates energy consumption JSON files from Enedis (ERDF) consumption data
-collected via their  website (API).
-"""
+"""Retrieves energy consumption data from your Enedis (ERDF) account."""
 
+# Linkindle - Linky energy consumption curves on a Kindle display.
+# Copyright (C) 2016 Baptiste Candellier
+# corrected (C) 2017 epierre
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -20,11 +22,12 @@ collected via their  website (API).
 import base64
 import requests
 
-LOGIN_BASE_URI = 'https://espace-client-connexion.erdf.fr'
-API_BASE_URI = 'https://espace-client-particuliers.erdf.fr/group/espace-particuliers'
+LOGIN_BASE_URI = 'https://espace-client-connexion.enedis.fr'
+API_BASE_URI = 'https://espace-client-particuliers.enedis.fr/group/espace-particuliers'
 
 API_ENDPOINT_LOGIN = '/auth/UI/Login'
-API_ENDPOINT_DATA = '/jeconsultetelechargemesdonneesdeconsommation'
+API_ENDPOINT_HOME = '/home'
+API_ENDPOINT_DATA = '/suivi-de-consommation'
 
 DATA_NOT_REQUESTED = -1
 DATA_NOT_AVAILABLE = -2
@@ -36,57 +39,74 @@ class LinkyLoginException(Exception):
 def login(username, password):
     """Logs the user into the Linky API.
     """
+    session = requests.Session()
+
     payload = {'IDToken1': username,
                'IDToken2': password,
                'SunQueryParamsString': base64.b64encode(b'realm=particuliers'),
                'encoded': 'true',
                'gx_charset': 'UTF-8'}
 
-    req = requests.post(LOGIN_BASE_URI + API_ENDPOINT_LOGIN, data=payload, allow_redirects=False)
-    session_cookie = req.cookies.get('iPlanetDirectoryPro')
+    #print (payload)
 
-    if session_cookie is None:
+    req = session.post(LOGIN_BASE_URI + API_ENDPOINT_LOGIN, data=payload, allow_redirects=False)
+
+    if not 'iPlanetDirectoryPro' in session.cookies:
         raise LinkyLoginException("Login unsuccessful. Check your credentials.")
 
-    return session_cookie
+    return session
 
-def get_data_per_hour(token, start_date, end_date):
-    """Retreives hourly energy consumption data."""
-    return _get_data(token, 'urlCdcHeure', start_date, end_date)
+def get_data_per_hour(session, start_date, end_date):
+    """Retrieves hourly energy consumption data."""
+    return _get_data(session, 'urlCdcHeure', start_date, end_date)
 
-def get_data_per_day(token, start_date, end_date):
-    """Retreives daily energy consumption data."""
-    return _get_data(token, 'urlCdcJour', start_date, end_date)
+def get_data_per_day(session, start_date, end_date):
+    """Retrieves daily energy consumption data."""
+    return _get_data(session, 'urlCdcJour', start_date, end_date)
 
-def get_data_per_month(token, start_date, end_date):
-    """Retreives monthly energy consumption data."""
-    return _get_data(token, 'urlCdcMois', start_date, end_date)
+def get_data_per_month(session, start_date, end_date):
+    """Retrieves monthly energy consumption data."""
+    return _get_data(session, 'urlCdcMois', start_date, end_date)
 
-def get_data_per_year(token):
-    """Retreives yearly energy consumption data."""
-    return _get_data(token, 'urlCdcAn')
+def get_data_per_year(session):
+    """Retrieves yearly energy consumption data."""
+    return _get_data(session, 'urlCdcAn')
 
-def _get_data(token, resource_id, start_date=None, end_date=None):
-    prefix = '_lincspartdisplaycdc_WAR_lincspartcdcportlet_INSTANCE_partlincspartcdcportlet_'
+def _get_data(session, resource_id, start_date=None, end_date=None):
+    iden = 'lincspartdisplaycdc_WAR_lincspartcdcportlet'
 
     # We send the session token so that the server knows who we are
-    cookies = {'iPlanetDirectoryPro': token}
     payload = {
-        prefix + 'dateDebut': start_date,
-        prefix + 'dateFin': end_date
+        '_' + iden + '_dateDebut': start_date,
+        '_' + iden + '_dateFin': end_date
     }
+
     params = {
-        'p_p_id': 'lincspartdisplaycdc_WAR_lincspartcdcportlet_INSTANCE_partlincspartcdcportlet',
+        'p_p_id': iden,
         'p_p_lifecycle': 2,
         'p_p_state': 'normal',
         'p_p_mode': 'view',
         'p_p_resource_id': resource_id,
         'p_p_cacheability': 'cacheLevelPage',
         'p_p_col_id': 'column-1',
-        'p_p_col_pos': 1,
-        'p_p_col_count': 3
+        'p_p_col_count': 2
     }
 
-    req = requests.post(API_BASE_URI + API_ENDPOINT_DATA, \
-                        allow_redirects=False, cookies=cookies, data=payload, params=params)
+    #print (params)
+    #print (payload)
+
+
+    req = session.post(API_BASE_URI + API_ENDPOINT_DATA, \
+                        allow_redirects=False, data=payload, params=params)
+
+    if req.status_code >= 300 and req.status_code < 400:
+        # So... apparently, we may need to do that once again if we hit a 302
+        # ¯\_(ツ)_/¯
+        req = session.post(API_BASE_URI + API_ENDPOINT_DATA, \
+                            allow_redirects=False, data=payload, params=params)
+
+    #print (req.status_code)
+    #print (req.headers)
+    #print (req.content)
+
     return req.json()
