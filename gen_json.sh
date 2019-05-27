c9eb34566aed709fb60d0876a6b27ef1922b4c71
François Lecoufle <flecoufle@gmail.com>
2019-05-27 14:07:37 +0200

    update

--- gen_json.sh
+++ gen_json.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
-#export LINKY_USERNAME="prenom.nom@mail.com"
-#export LINKY_PASSWORD="password"
-
-#BASE_DIR="/var/www/html/plugins/script/core/ressources/linky"
-BASE_DIR="./"
-
+BASE_DIR="/home/pi/jeedom_linky"
 export BASE_DIR
 
+. $BASE_DIR/credentials
 python3 $BASE_DIR/linky_json.py -o "$BASE_DIR" >> $BASE_DIR/linky.log 2>&1
